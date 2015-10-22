c26161ff685b80f4e22c6424d66d5039b60309f7
Arne Böckmann <arne.boeckmann@dfki.de>
2015-10-22 15:33:24 +0200

    added end_line to interface

--- interface.py
+++ interface.py
@@ -1,8 +1,9 @@
 __author__ = 'aboeckmann'
 
 class Marker:
-    def __init__(self, line, start_col, end_col):
-        self.line = line
+    def __init__(self, start_line, end_line, start_col, end_col):
+        self.start_line = start_line
+        self.end_line = end_line
         self.start_col = start_col
         self.end_col = end_col
 
