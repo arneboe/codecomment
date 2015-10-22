79d3e43085a221b6e1084f060449126e76e73345
Arne Böckmann <arne.boeckmann@dfki.de>
2015-10-22 13:55:47 +0200

    basic adding of comments works

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -3,42 +3,81 @@ __author__ = 'aboeckmann'
 from PyQt4.QtGui import QMainWindow, QFileDialog, QFont, QListWidgetItem
 from PyQt4 import uic
 from Highlighter import Highlighter
+from interface import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
 
-        # Set up the user interface from Designer.
+        # Set up the user interface from qt designer file
         self.ui = uic.loadUi('gui/ui_MainWindow.ui')
 
-
+        #initialize the highlighter
         font = QFont()
         font.setFamily('Courier')
         font.setFixedPitch(True)
         font.setPointSize(10)
         self.ui.plainTextEditCode.setFont(font)
         self.highlighter = Highlighter(self.ui.plainTextEditCode.document())
-        self.files = []
 
         self.ui.actionOpen.triggered.connect(self.open)
+        self.ui.actionAdd_Selection.setEnabled(False)#is disbled initially because no comment is selected on startup
+        self.ui.actionAdd_Comment.triggered.connect(self.addComment)
 
         self.ui.listWidgetFiles.currentItemChanged.connect(self.fileListSelectedItemChanged)
+        self.ui.listWidgetComments.currentItemChanged.connect(self.selectedCommentChanged)
+        self.ui.plainTextEditComment.textChanged.connect(self.commentTextChanged)
 
+        self.next_comment_no = 0
+        self.data = Data(0)
+        self.current_comment = None #the currently selected comment, if any
 
         self.ui.show();
 
     def open(self):
-        '''
-        is called when the user clicks file->open
-        '''
-        self.files = QFileDialog.getOpenFileNames(self, 'Open file', '.')
-        for f in self.files:
+        #is called when the user clicks open
+        files = QFileDialog.getOpenFileNames(self, 'Open file', '.')
+        for f in files:
             item = QListWidgetItem(f)
             self.ui.listWidgetFiles.addItem(item)
-        if(len(self.files) > 0):
+            self.data.add_file(File(f))
+        if(len(files) > 0):
             self.ui.listWidgetFiles.setCurrentItem(self.ui.listWidgetFiles.item(0))
 
     def fileListSelectedItemChanged(self, curr, prev):
+        #is called whenever tan item is selecetd in the file list
         f = open(curr.text(), 'r')
         self.ui.plainTextEditCode.setPlainText(f.read())
+
+    def get_current_file_path(self):
+        return self.ui.listWidgetFiles.currentItem().text()
+
+    def get_current_comment_index(self):
+        return self.ui.listWidgetComments.currentRow()
+
+    def addComment(self):
+        #is called whenever the user clicks "add comment"
+        initial_comment_text = "comment #" + str(self.next_comment_no)
+        self.next_comment_no += 1
+        file = self.data.get_file_by_path(self.get_current_file_path())
+        file.add_comment(Comment(initial_comment_text))
+        item = QListWidgetItem(initial_comment_text)
+        self.ui.listWidgetComments.addItem(item)
+        self.ui.listWidgetComments.setCurrentItem(item)
+
+        #as soon as we have one comment, we can enable the "add selection" button
+        self.ui.actionAdd_Selection.setEnabled(True)
+
+    def selectedCommentChanged(self, curr, prev):
+        #called whenever another comment is selected
+        current_file = self.data.get_file_by_path(self.get_current_file_path())
+        self.current_comment = current_file.get_comment(self.get_current_comment_index())
+        comment_text = self.current_comment.get_text()
+        self.ui.plainTextEditComment.blockSignals(True) #otherwise it would cause a textChanged() event which would overwrite the data
+        self.ui.plainTextEditComment.setPlainText(comment_text)
+        self.ui.plainTextEditComment.blockSignals(False)
+
+    def commentTextChanged(self):
+        new_text = self.ui.plainTextEditComment.document().toPlainText()
+        self.current_comment.set_text(new_text)
--- interface.py
+++ interface.py
@@ -18,6 +18,9 @@ class Comment:
     def set_text(self, text):
         self.text = text
 
+    def get_text(self):
+        return self.text
+
 class File:
     def __init__(self, path="", comments=[]):
         self.path = path
@@ -29,6 +32,9 @@ class File:
     def set_path(self, path):
         self.path = path
 
+    def get_comment(self, i):
+        return self.comments[i]
+
 class Data:
     def __init__(self, group_no, files = []):
         self.group_no = group_no
@@ -36,3 +42,9 @@ class Data:
 
     def add_file(self, file):
         self.files.append(file)
+
+    def get_file_by_path(self, path):
+        for f in self.files:
+            if f.path == path:
+                return f
+        raise ValueError(path + "does not exist")
