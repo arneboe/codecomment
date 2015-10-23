326cd15c02243061a71bc0d9a1cb489c9089452c
Arne Böckmann <arne.boeckmann@dfki.de>
2015-10-23 12:09:50 +0200

    forgot file

--- main2.py
+++ main2.py
@@ -1,1 +1,48 @@
 __author__ = 'aboeckmann'
+
+from interface import *
+
+#just for testing
+
+
+
+group_no = 1
+d = Data(group_no)
+
+file_1 = File("./test_code/ConfigurationHelper.hpp")
+file_2 = File("./test_code/test_transform_graph.cpp")
+d.add_file(file_1)
+d.add_file(file_2)
+
+f1_comment_1 = Comment("test 123 hallo bla bla")
+f1_comment_2 = Comment("aaaaaaaaaa wdew qdwq dwqd wqd frtg ftghf th fth gzhvgz h fh")
+file_1.add_comment(f1_comment_1)
+file_1.add_comment(f1_comment_2)
+
+f2_comment_1 = Comment("bla blablab blabla blablab blabla blablab blabla blablab bla")
+f2_comment_2 = Comment("aaaaa bbbb cccc ddd eee fff ggg")
+file_2.add_comment(f2_comment_1)
+file_2.add_comment(f2_comment_2)
+
+f1_c1_marker_1 = Marker(0, 0, 3, 12)
+f1_c1_marker_2 = Marker(2, 3, 0, 5)
+f1_comment_1.add_marker(f1_c1_marker_1)
+f1_comment_1.add_marker(f1_c1_marker_2)
+
+f1_c2_marker_1 = Marker(21, 21, 3, 20)
+f1_comment_2.add_marker(f1_c2_marker_1)
+
+f2_c1_marker_1 = Marker(57, 57, 10, 20)
+f2_comment_1.add_marker(f2_c1_marker_1)
+
+f2_c2_marker_1 = Marker(268, 268, 0, 10)
+f2_comment_2.add_marker(f2_c2_marker_1)
+
+print("bla")
+
+
+
+'''
+class Marker:
+    def __init__(self, start_line, end_line, start_col, end_col):
+'''
